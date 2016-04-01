f7c3cdcbadd72f90d9eee24e0a593dcccca38152
Matias Rondeau <matu_dmr@hotmail.com>
2016-04-01 16:44:18 -0300

    Fuerza Bruta ejercicios

---
+++ fuerzaBruta2.hs
@@ -0,0 +1,44 @@
+--Escriba programas Haskell que realicen las siguientes tareas:
+--• dada una lista, genere todas las permutaciones de la misma,
+--• dado un conjunto representado como una lista, genere todos los subconjuntos del mismo,
+--• dada una lista, genere todas las sublistas de la misma.
+
+--Permutacion de una lista de elementos.
+perms::Ord a =>[a]->[[a]]
+perms [] = [[]]
+perms (x:xs) = concat [move  x ys |ys <- perms xs ] 
+
+--mueve un elemento en una lista formando listas de listas.
+move :: a->[a]->[[a]]
+move x [] = [[x]]
+move x (y:ys)= (x:y:ys) : [y:zs | zs <- move x ys]
+
+
+--subconjuntos de un conjunto
+subconjuntos :: [a] -> [[a]]
+subconjuntos []     = [[]]
+subconjuntos (x:xs) = [x:ys | ys <- subconjuntos xs] ++ subconjuntos xs
+
+
+--Generar todas las sublistas de una lista
+--sublista:: [a] -> [[a]]
+--sublista [] = [[]]
+--sublista (x:xs) =
+
+--Escriba un programa Haskell que implemente el algoritmo de ordenamiento SlowSort, que ordena
+--una lista de elementos (por ejemplo, de enteros) mediante la generaci ́on de la lista de todas las permuta-
+--ciones de la lista original, y filtrando aquella que est ́a ordenada.Utilice su cuenta en codeboard para acceder
+--al template de este algoritmo donde podr ́
+--a encontrar tambi ́en una serie de test para probar su programa.
+
+
+
+slowSort::Ord a =>[a]-> [a]
+slowSort [] = []
+slowSort (x:xs) =  head ([x | x <- perms (x:xs), orden x]) 
+
+
+orden:: Ord a => [a]->Bool
+orden [] = True
+orden [x] = True
+orden (x:y:xs) = (x<=y)  && (orden (y:xs))
